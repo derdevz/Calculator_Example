10ecd6df25b7cab92a48d13a077624d09f8ab76a
derdevz <220624386+derdevz@users.noreply.github.com>
2025-10-16 16:21:23 +0300

    v0.0 Basic Inputs
    Variables were created. A system that requested user input was implemented. The foundations for a system that would determine the action to be taken were laid.

---
+++ Calculator_Example.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // Variables are stored here.
+    
+    string _calculator  = "Hesap makinesi";
+    float _firstNumber, _secondNumber;
+    char _symbol;
+
+    // inputs requested from us.
+    cout << "Sayi giriniz" << endl;
+    cin >> _firstNumber;
+
+    cout << "Işlem türü seçiniz" << endl;
+    cin >> _symbol;
+
+    cout << "Sayi giriniz" << endl;
+    cin >> _secondNumber;
+
+    // With the switch case structure, we determine which operation we want and carry out the desired operation accordingly.
+    switch (_symbol)
+    {
+    case 
+        break;
+    }
+    return 0;
+}
